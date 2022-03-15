c664be827bd949c1dd3dfefa1cacdb53efc16584
Belal Gawish <blalbmw721@gmail.com>
2022-03-15 07:24:04 +0300

    Init comit on second branch
    initial commit

--- Primitive Shapes/Assets/Scripts/PrimitiveType.cs
+++ Primitive Shapes/Assets/Scripts/PrimitiveType.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Drawing;
 
 
 public class PrimitiveType : MonoBehaviour
@@ -11,23 +10,45 @@ public class PrimitiveType : MonoBehaviour
     public Text nameText;
     public Text colorText;
     public Text speedText;
-    public float speed = 5.0f;
+    public float speed;
+    public Material material;
+    private string materialName;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        material.color = Color.white;
+        ShowInfo();
+    }
+
+    void SetColor()
+    {
+        material.color = Color.green;
+    }
+
+    void SetRotationSpeed()
+    {
+        speed = 50.0f;
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    void ShowInfo()
+    {
         if (Input.GetKey(KeyCode.Mouse0))
         {
+            string materialName = material.name;
             nameText.text = "Name: " + gameObject.name;
-            colorText.text = "Color: Green";
+            colorText.text = "Color: " + materialName;
             speedText.text = "Speed: " + speed;
 
-            transform.Rotate(Vector3.right * speed * Time.deltaTime);
+            SetColor();
+            SetRotationSpeed();
         }
     }
 }
